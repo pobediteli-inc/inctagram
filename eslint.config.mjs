9fe018f3b36695d0065ff2f27c89dad264b69589
umedjan kabiri <umedjan.kabiri@gmail.com>
2025-01-19 17:53:52 +0300

    Refactor ESLint config to enhance TypeScript support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,34 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import tseslint from "@typescript-eslint/eslint-plugin";
 import nextEslint from "eslint-config-next";
+import pluginReact from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
   {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    files: ["**/*.{js,mjs,cjs,ts,tsx,jsx}"],
     languageOptions: {
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+        project: "./tsconfig.json",
+      },
       globals: globals.browser,
     },
     rules: {
       "react/react-in-jsx-scope": "off",
       "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-      "no-console": "warn",
+      "no-console": "warn", // Warn for console logs
     },
   },
+  // Recommended rules for vanilla JS
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   nextEslint(),
 ];
