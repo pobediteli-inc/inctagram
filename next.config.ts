1e805a778aea37aa31e105c3c53f502ad9c49378
umedjan kabiri <umedjan.kabiri@gmail.com>
2025-01-19 17:53:21 +0300

    Add webpack aliases to Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
+import path from "node:path";
 
+// Define your Next.js configuration
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      app: path.resolve(__dirname, "src/app"),
+      common: path.resolve(__dirname, "src/common"),
+      features: path.resolve(__dirname, "src/features"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
