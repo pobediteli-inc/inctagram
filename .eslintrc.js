d8bb7eabe8442076c5f5735bff360a1792473e96
umedjan kabiri <umedjan.kabiri@gmail.com>
2025-01-20 22:53:00 +0300

    Add ESLint configuration with TypeScript and React support

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: "./tsconfig.json",
+  },
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  plugins: ["@typescript-eslint", "react"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "next/core-web-vitals",
+  ],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "no-console": "warn",
+  },
+};
