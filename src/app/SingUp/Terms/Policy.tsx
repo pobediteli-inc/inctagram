a97b57eac766bc011d5270b332223e945ce04370
umedjan kabiri <umedjan.kabiri@gmail.com>
2025-01-24 02:24:43 +0300

    SCRUM-3 feat: Add Terms of Service and Policy components with styles

---
+++ src/app/SingUp/Terms/Policy.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import stl from "app/SingUp/Terms/Terms.module.css";
+
+
+const Policy = () => {
+  return (
+    <div className={stl.termsWrapper}>
+      <p>lorem ipsum</p>
+    </div>
+  );
+};
+
+export default Policy;
