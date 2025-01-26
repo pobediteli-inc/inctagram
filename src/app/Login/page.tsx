339eaaa14cb56a37462a30d169424e6d982e99bf
umedjan kabiri <umedjan.kabiri@gmail.com>
2025-01-26 18:02:04 +0300

    SCRUM-10 feat: create Login component with styles

---
+++ src/app/Login/page.tsx
@@ -0,0 +1,63 @@
+import stl from "./Login.module.css";
+import { Typography } from "common/components/Typography/Typography";
+import Link from "next/link";
+import GoogleSvgRepoCom1 from "common/components/SVGComponents/GoogleSvgRepoCom1";
+import GithubSvgRepoCom31 from "common/components/SVGComponents/GithubSvgRepoCom31";
+import { TextField } from "common/components/TextField/TextField";
+import { Button } from "common/components/Button/Button";
+
+export default function Login() {
+  return (
+    <div className={stl.loginWrapper}>
+      <Typography variant={"h1"} className={stl.signInHeader}>
+        Sign In
+      </Typography>
+      <div className={stl.socialIcons}>
+        <Link href={"https://www.google.com"} target={"_blank"}>
+          <GoogleSvgRepoCom1 width={36} height={36} />
+        </Link>
+        <Link href={"https://www.github.com"} target={"_blank"}>
+          <GithubSvgRepoCom31 width={36} height={36} color={"white"} />
+        </Link>
+      </div>
+      <div className={stl.forms}>
+        <div className={stl.fieldWrapper}>
+          <TextField
+            textFieldClassName={stl.email}
+            variant={"standard"}
+            type={"email"}
+            placeholder={"example@example.com"}
+            label={"Email"}
+            labelPosition={"top"}
+          />
+          <TextField
+            textFieldClassName={stl.password}
+            variant={"standard"}
+            type={"password"}
+            placeholder={"**********"}
+            label={"Password"}
+            labelPosition={"top"}
+          />
+        </div>
+      </div>
+      <div className={stl.buttonsWrapper}>
+        <Typography variant={"regular_14"} className={stl.forgotPassword}>
+          <Link href={"#"} className={stl.forgotLink}>
+            Forgot password?
+          </Link>
+        </Typography>
+        <div className={stl.buttonWrapper}>
+          <Button variant={"primary"} className={stl.signInButton}>
+            Sign In
+          </Button>
+        </div>
+        <Typography variant={"regular_16"}>Don&#39;t have an account?</Typography>
+        <div className={stl.buttonWrapper}>
+          <Button variant={"outlined"} className={stl.signInButton} asChild>
+            <Link href={"/"}>Sign Up</Link>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
