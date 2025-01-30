006add519be6694f71cb5603c88506865053b277
Slava Shedu <frontend.incubator@gmail.com>
2025-01-30 13:41:43 +0100

    SCRUM-11 feat: add basic styles

--- src/common/components/Pagination/Pagination.tsx
+++ src/common/components/Pagination/Pagination.tsx
@@ -1,33 +1,55 @@
+"use client";
 import { useState } from "react";
-import { useRouter } from "next/router";
 
-type PaginationProps = {
-  totalPages: number;
+import styles from "./Pagination.module.scss";
+import { clsx } from "clsx";
+import SvgArrowIosBack from "common/components/SVGComponents/ArrowIosBack";
+import SvgArrowIosForward from "common/components/SVGComponents/ArrowIosForward";
+import { useRouter } from "next/navigation";
+
+export type PaginationProps = {
+  totalPages?: number;
 };
 
-const Pagination = ({ totalPages }: PaginationProps) => {
+const Pagination = ({ totalPages = 10 }: PaginationProps) => {
   const [currentPage, setCurrentPage] = useState(1);
   const router = useRouter();
 
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
-    router.push(`?page=${page}`, undefined, { shallow: true });
+    router.push(`?page=${page}`);
   };
 
   return (
-    <div className="pagination">
-      <button onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}>
-        Previous
+    <div className={styles.paginationContainer}>
+      <button
+        onClick={() => handlePageChange(currentPage - 1)}
+        disabled={currentPage === 1}
+        className={clsx(styles.navigationButton, { [styles.disabled]: currentPage === 1 })}
+      >
+        <SvgArrowIosBack width={16} height={16} color={currentPage === 1 ? "var(--dark-100)" : "var(--light-100)"} />
       </button>
 
       {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
-        <button key={page} onClick={() => handlePageChange(page)}>
+        <button
+          key={page}
+          onClick={() => handlePageChange(page)}
+          className={`${styles.basicButton} ${page === currentPage ? styles.active : ""} typography-variant--regular_14`}
+        >
           {page}
         </button>
       ))}
 
-      <button onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages}>
-        Next
+      <button
+        onClick={() => handlePageChange(currentPage + 1)}
+        disabled={currentPage === totalPages}
+        className={clsx(styles.navigationButton, { [styles.disabled]: currentPage === totalPages })}
+      >
+        <SvgArrowIosForward
+          width={16}
+          height={16}
+          color={currentPage === totalPages ? "var(--dark-100)" : "var(--light-100)"}
+        />
       </button>
     </div>
   );
