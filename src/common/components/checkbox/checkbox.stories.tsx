c68769f6a866d7cc90af1a42af45a401ebf17ce8
umedjan kabiri <umedjan.kabiri@gmail.com>
2025-02-03 01:16:00 +0300

    SCRUM-14 feat: refactor Checkbox component, add storybook, update styles

---
+++ src/common/components/checkbox/checkbox.stories.tsx
@@ -0,0 +1,55 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Checkbox } from "./checkbox";
+
+const meta: Meta<typeof Checkbox> = {
+  title: "Components/RadixCheckbox",
+  component: Checkbox,
+  tags: ["autodocs"],
+  argTypes: {
+    containerClassName: {
+      table: { disable: true },
+    },
+    labelClassName: {
+      table: { disable: true },
+    },
+    labelPosition: {
+      control: { type: "select" },
+      options: ["top", "right", "bottom", "left", "center"],
+    },
+    checked: { control: "boolean" },
+    onCheckedChange: { action: "checked" },
+    disabled: { control: "boolean" },
+  },
+  parameters: {
+    layout: "labelPosition",
+  },
+} satisfies Meta<typeof Checkbox>;
+
+export default meta;
+type Story = StoryObj<typeof Checkbox>;
+
+export const CheckedTrue: Story = {
+  args: {
+    label: "Checked Checkbox",
+    labelPosition: "center",
+    checked: true,
+    disabled: false,
+  },
+};
+
+export const CheckedFalse: Story = {
+  args: {
+    label: "Unchecked Checkbox",
+    labelPosition: "center",
+    checked: false,
+    disabled: false,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    label: "Disabled Checkbox",
+    labelPosition: "center",
+    disabled: true,
+  },
+};
