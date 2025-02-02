5f78e85a4a8f85b28c9b631ac5a95a00d235d1f5
umedjan kabiri <umedjan.kabiri@gmail.com>
2025-02-03 01:41:33 +0300

    SCRUM-14 refactor: rename storybook in left sidebar

--- src/common/components/checkbox/checkbox.stories.tsx
+++ src/common/components/checkbox/checkbox.stories.tsx
@@ -2,7 +2,7 @@ import { Meta, StoryObj } from "@storybook/react";
 import { Checkbox } from "./checkbox";
 
 const meta: Meta<typeof Checkbox> = {
-  title: "Components/RadixCheckbox",
+  title: "Components/Checkbox",
   component: Checkbox,
   tags: ["autodocs"],
   argTypes: {
