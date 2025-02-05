dcd4dedbbd9b7585eb31aa5e88c6c28928c0bd62
Slava Shedu <frontend.incubator@gmail.com>
2025-02-05 11:40:02 +0100

    SCRUM-11 refactor: import as s instead of styles

--- src/common/components/Pagination/Pagination.tsx
+++ src/common/components/Pagination/Pagination.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import styles from "./Pagination.module.scss";
+import s from "./Pagination.module.scss";
 import { clsx } from "clsx";
 import SvgArrowIosBack from "common/components/SVGComponents/ArrowIosBack";
 import SvgArrowIosForward from "common/components/SVGComponents/ArrowIosForward";
@@ -34,11 +34,11 @@ const Pagination = ({ totalPages }: PaginationProps) => {
   };
 
   return (
-    <div className={styles.paginationContainer}>
+    <div className={s.paginationContainer}>
       <button
         onClick={() => handlePageChange(currentPage - 1)}
         disabled={currentPage === 1}
-        className={clsx(styles.navigationButton, { [styles.disabled]: currentPage === 1 })}
+        className={clsx(s.navigationButton, { [s.disabled]: currentPage === 1 })}
       >
         <SvgArrowIosBack width={16} height={16} color={currentPage === 1 ? "var(--dark-100)" : "var(--light-100)"} />
       </button>
@@ -50,15 +50,15 @@ const Pagination = ({ totalPages }: PaginationProps) => {
               key={page}
               onClick={() => handlePageChange(page)}
               className={clsx(
-                styles.basicButton,
-                { [styles.active]: page === currentPage },
+                s.basicButton,
+                { [s.active]: page === currentPage },
                 "typography-variant--regular_14"
               )}
             >
               {page}
             </button>
           ) : (
-            <span className={styles.spanContainer}>{page}</span>
+            <span className={s.spanContainer}>{page}</span>
           )}
         </React.Fragment>
       ))}
@@ -66,7 +66,7 @@ const Pagination = ({ totalPages }: PaginationProps) => {
       <button
         onClick={() => handlePageChange(currentPage + 1)}
         disabled={currentPage === lastPage}
-        className={clsx(styles.navigationButton, { [styles.disabled]: currentPage === totalPages })}
+        className={clsx(s.navigationButton, { [s.disabled]: currentPage === totalPages })}
       >
         <SvgArrowIosForward
           width={16}
@@ -75,10 +75,10 @@ const Pagination = ({ totalPages }: PaginationProps) => {
         />
       </button>
 
-      <div className={clsx(styles.selectContainer, "typography-variant--regular_14")}>
+      <div className={clsx(s.selectContainer, "typography-variant--regular_14")}>
         <span>Show</span>
         <select
-          className={styles.selectBox}
+          className={s.selectBox}
           value={pageSize}
           onChange={(e) => onPageSizeChange(Number(e.target.value))}
         >
