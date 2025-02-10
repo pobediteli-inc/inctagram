a0fe7b3e3aa1a19cc851a44a4bfef22a83d61f6c
Slava Shedu <frontend.incubator@gmail.com>
2025-02-10 10:28:45 +0100

    SCRUM-11 refactor: add Typography to error message

--- src/common/components/recaptcha/recaptcha.tsx
+++ src/common/components/recaptcha/recaptcha.tsx
@@ -2,6 +2,7 @@
 
 import ReCAPTCHA from "react-google-recaptcha";
 import s from "./recaptcha.module.scss";
+import { Typography } from "../typography/typography";
 
 type ReCaptchaPropsType = {
   sitekey: string;
@@ -13,7 +14,7 @@ const ReCaptcha = ({ sitekey, onVerify, error }: ReCaptchaPropsType) => {
   return (
     <div className={error ? s.errorContainer : ""}>
       <ReCAPTCHA sitekey={sitekey} theme={"dark"} className={s.recaptcha} onChange={onVerify} />
-      {error ? <p className={s.errorMessage}>Please verify that you are not a robot</p> : ""}
+      {error ? <Typography asChild={true} className={s.errorMessage}><p>Please verify that you are not a robot</p></Typography> : ""}
     </div>
   );
 };
