f800ad165eae2df10e9b587ede45896277b25f6e
Slava Shedu <frontend.incubator@gmail.com>
2025-02-10 21:41:11 +0100

    SCRUM-17 feat: realisation of baseModal

---
+++ src/common/components/modal/baseModal/baseModal.tsx
@@ -0,0 +1,37 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import clsx from "clsx";
+import s from "./baseModal.module.css";
+import { ComponentPropsWithoutRef } from "react";
+import SvgClose from "assets/icons/Close";
+import { Typography } from "../../typography/typography";
+
+type Props = {
+  open: boolean
+  onClose: () => void
+  modalTitle: string
+} & ComponentPropsWithoutRef<'div'>
+
+export const BaseModal = ({modalTitle, onClose, open, children, className, ...rest}: Props) => (
+  <Dialog.Root open={open} onOpenChange={onClose} {...rest}>
+    <Dialog.Portal>
+      <Dialog.Overlay className={s.Overlay}/>
+      <Dialog.Content className={clsx(s.Content, className)}>
+
+        <Dialog.Title className={s.Title}>
+          <Typography variant={"h1"} color={"light"}>{modalTitle}</Typography>
+        </Dialog.Title>
+
+        <hr className={s.separator}/>
+
+        {children}
+
+        <Dialog.Close asChild>
+          <button className={s.IconButton} aria-label="Close">
+            <SvgClose width={"24px"} height={"24px"}/>
+          </button>
+        </Dialog.Close>
+
+      </Dialog.Content>
+    </Dialog.Portal>
+  </Dialog.Root>
+);
