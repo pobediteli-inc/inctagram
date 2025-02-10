464f1a8f41594b859b3f6c8f11b384fbb8051768
umedjan kabiri <umedjan.kabiri@gmail.com>
2025-02-10 05:22:13 +0300

    SCRUM-14 refactor: add isAuth prop for Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable}`}>
-        <Header />
+        <Header isAuth={false} />
         <main>{children}</main>
       </body>
     </html>
