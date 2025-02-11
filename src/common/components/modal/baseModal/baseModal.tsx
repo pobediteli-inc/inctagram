6e20fc107d49da22afc808460de138a900aae7bc
Slava Shedu <frontend.incubator@gmail.com>
2025-02-11 11:29:11 +0100

    SCRUM-17 refactor: format the baseModal code

--- src/common/components/modal/baseModal/baseModal.tsx
+++ src/common/components/modal/baseModal/baseModal.tsx
@@ -6,31 +6,28 @@ import SvgClose from "assets/icons/Close";
 import { Typography } from "../../typography/typography";
 
 type Props = {
-  open: boolean
-  onClose: () => void
-  modalTitle: string
-} & ComponentPropsWithoutRef<'div'>
+  open: boolean;
+  onClose: () => void;
+  modalTitle: string;
+} & ComponentPropsWithoutRef<"div">;
 
-export const BaseModal = ({modalTitle, onClose, open, children, className, ...rest}: Props) => (
+export const BaseModal = ({ modalTitle, onClose, open, children, className, ...rest }: Props) => (
   <Dialog.Root open={open} onOpenChange={onClose} {...rest}>
     <Dialog.Portal>
-      <Dialog.Overlay className={s.Overlay}/>
+      <Dialog.Overlay className={s.Overlay} />
       <Dialog.Content className={clsx(s.Content, className)}>
-
         <Dialog.Title className={s.Title}>
-          <Typography variant={"h1"} color={"light"}>{modalTitle}</Typography>
+          <Typography variant={"h1"} color={"light"}>
+            {modalTitle}
+          </Typography>
         </Dialog.Title>
-
-        <hr className={s.separator}/>
-
+        <hr className={s.separator} />
         {children}
-
         <Dialog.Close asChild>
           <button className={s.IconButton} aria-label="Close">
-            <SvgClose width={"24px"} height={"24px"}/>
+            <SvgClose width={"24px"} height={"24px"} />
           </button>
         </Dialog.Close>
-
       </Dialog.Content>
     </Dialog.Portal>
   </Dialog.Root>
