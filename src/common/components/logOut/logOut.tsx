2635af7457ac5418b20b4a461a95f6a73df92207
Slava Shedu <frontend.incubator@gmail.com>
2025-02-12 19:04:49 +0100

    SCRUM-17 fix: format code

---
+++ src/common/components/logOut/logOut.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { LogOutOutline } from "../../../assets/icons";
+import { useState } from "react";
+import { LogOutModal } from "../modal/logOutModal/logOutModal";
+import s from "./logOut.module.scss";
+import { Typography } from "../typography/typography";
+
+export const LogOut = () => {
+  const [showModal, setShowModal] = useState(false);
+  const account = "Epam@epam.com";
+
+  const toggleModal = () => setShowModal((prev) => !prev);
+
+  const logOutHandler = () => {
+    // logout().then(res => {})
+  };
+
+  return (
+    <div>
+      <div className={s.container} onClick={toggleModal} aria-label="Log out">
+        <LogOutOutline width={24} height={24} color={"var(--light-100)"} className={s.Icon} />
+        <Typography variant={"medium_14"} color={"light"}>
+          Log Out
+        </Typography>
+      </div>
+      <LogOutModal open={showModal} onClose={toggleModal} email={account} onLogout={logOutHandler} />
+    </div>
+  );
+};
