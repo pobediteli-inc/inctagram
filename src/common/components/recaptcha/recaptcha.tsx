82efeab22b03ff4147d8dac03542355f71071f56
Slava Shedu <frontend.incubator@gmail.com>
2025-02-12 19:37:49 +0100

    SCRUM-17 fix: delete font-size from .scss for message and use Typography component instead

--- src/common/components/recaptcha/recaptcha.tsx
+++ src/common/components/recaptcha/recaptcha.tsx
@@ -14,7 +14,13 @@ const ReCaptcha = ({ sitekey, onVerify, error }: ReCaptchaPropsType) => {
   return (
     <div className={error ? s.errorContainer : ""}>
       <ReCAPTCHA sitekey={sitekey} theme={"dark"} className={s.recaptcha} onChange={onVerify} />
-      {error ? <Typography asChild={true} className={s.errorMessage}><p>Please verify that you are not a robot</p></Typography> : ""}
+      {error ? (
+        <Typography asChild={true} variant={"small"} className={s.errorMessage}>
+          <p>Please verify that you are not a robot</p>
+        </Typography>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
