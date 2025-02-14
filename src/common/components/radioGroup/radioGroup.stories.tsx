c4185f8b4fc8023953439372229fa8fec07f7c87
umedjan kabiri <umedjan.kabiri@gmail.com>
2025-02-15 00:33:28 +0300

    SCRUM-14 refactor: delete options from disabled state

--- src/common/components/radioGroup/radioGroup.stories.tsx
+++ src/common/components/radioGroup/radioGroup.stories.tsx
@@ -35,12 +35,8 @@ export const RadioGroupOptions: Story = {
 export const RadioGroupDisabled: Story = {
   args: {
     label: "radio group label",
-    options: [
-      { value: "option1", label: "option1" },
-      { value: "option2", label: "option2" },
-      { value: "option3", label: "option3" },
-      { value: "option4", label: "option4" },
-    ],
+    options: [{ value: "option1", label: "option1" }],
+    labelPosition: "bottom",
     disabled: true,
   },
 };
