4321cfc0321d65724cf78020640bb317ec283f9a
eliseevais <irina.eliseeva1991@gmail.com>
2025-02-22 17:04:47 +0300

    SCRUM-9: refactor SignUp, add registration logic, main case

--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -1,23 +1,36 @@
 "use client";
 
 import s from "app/signUp/signUp.module.css";
-import { Button, Checkbox, TextField, Typography } from "common/components";
+import { Button, TextField, Typography } from "common/components";
 import { Github, Google } from "assets/icons";
-import { useState } from "react";
 import Link from "next/link";
 import { useFormik } from "formik";
+import { useState } from "react";
+import { Cards } from "../../common/components/cards/cards";
 
 type FormValuesType = {
   username: string;
   email: string;
   password: string;
   passwordConfirm: string;
+  isAgree: boolean;
 };
 
 export default function SignUp() {
-  const [checked, setChecked] = useState<"indeterminate" | boolean>(false);
-
   const formik = useFormik({
+    initialValues: {
+      username: "",
+      email: "",
+      password: "",
+      passwordConfirm: "",
+      isAgree: false,
+    },
+    onSubmit: (values: FormValuesType, { resetForm }) => {
+      alert(JSON.stringify(values, null, 2));
+      setEmail(values.email);
+      resetForm();
+      setIsOpen(true);
+    },
     validate: (values) => {
       if (!values.username) {
         return {
@@ -66,19 +79,23 @@ export default function SignUp() {
           passwordConfirm: "The passwords must match",
         };
       }
-    },
-    initialValues: {
-      username: "",
-      email: "",
-      password: "",
-      passwordConfirm: "",
-    },
-    onSubmit: (values: FormValuesType, { resetForm }) => {
-      alert(JSON.stringify(values, null, 2));
-      resetForm();
-      setChecked(false);
+
+      if (!values.isAgree) {
+        return {
+          isAgree: "Agreement is required",
+        };
+      }
     },
   });
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [email, setEmail] = useState("");
+  const openPopup = () => {
+    setIsOpen(true);
+  };
+  const closePopup = () => {
+    setIsOpen(false);
+  };
 
   return (
     <div className={s.signUpWrapper}>
@@ -93,56 +110,72 @@ export default function SignUp() {
           <Github width={36} height={36} color={"white"} />
         </Link>
       </div>
+
       <div className={s.mainContent}>
         <form onSubmit={formik.handleSubmit}>
           <div className={s.forms}>
-            <TextField
-              textFieldClassName={s.username}
-              variant={"standard"}
-              type={"text"}
-              placeholder={"username"}
-              label={"Username"}
-              {...formik.getFieldProps("username")}
-            />
-            {formik.errors.username && formik.touched.username
-              ? (<div className={s.errorMessage}>{formik.errors.username}</div>)
-              : null}
+            <div className={s.form}>
+              <TextField
+                textFieldClassName={s.username}
+                variant={"standard"}
+                type={"text"}
+                placeholder={"username"}
+                label={"Username"}
+                {...formik.getFieldProps("username")}
+              />
+              {formik.errors.username && formik.touched.username ? (
+                <div className={s.errorMessage}>{formik.errors.username}</div>
+              ) : null}
+            </div>
 
-            <TextField
-              textFieldClassName={s.email}
-              variant={"standard"}
-              type={"email"}
-              placeholder={"example@example.com"}
-              label={"Email"}
-              {...formik.getFieldProps("email")}
-            />
-            {formik.errors.email && formik.touched.email ? <div>{formik.errors.email}</div> : null}
+            <div className={s.form}>
+              <TextField
+                textFieldClassName={s.email}
+                variant={"standard"}
+                type={"email"}
+                placeholder={"example@example.com"}
+                label={"Email"}
+                {...formik.getFieldProps("email")}
+              />
+              {formik.errors.email && formik.touched.email ? (
+                <div className={s.errorMessage}>{formik.errors.email}</div>
+              ) : null}
+            </div>
 
-            <TextField
-              textFieldClassName={s.password}
-              variant={"standard"}
-              type={"password"}
-              placeholder={"**********"}
-              label={"Password"}
-              {...formik.getFieldProps("password")}
-            />
-            {formik.errors.password && formik.touched.password ? <div>{formik.errors.password}</div> : null}
+            <div className={s.form}>
+              <TextField
+                textFieldClassName={s.password}
+                variant={"standard"}
+                type={"password"}
+                placeholder={"**********"}
+                label={"Password"}
+                {...formik.getFieldProps("password")}
+              />
+              {formik.errors.password && formik.touched.password ? (
+                <div className={s.errorMessage}>{formik.errors.password}</div>
+              ) : null}
+            </div>
 
-            <TextField
-              textFieldClassName={s.password}
-              variant={"standard"}
-              type={"password"}
-              placeholder={"**********"}
-              label={"Confirm password"}
-              {...formik.getFieldProps("passwordConfirm")}
-            />
-            {formik.errors.passwordConfirm && formik.touched.passwordConfirm ? (
-              <div>{formik.errors.passwordConfirm}</div>
-            ) : null}
+            <div className={s.form}>
+              <TextField
+                textFieldClassName={s.password}
+                variant={"standard"}
+                type={"password"}
+                placeholder={"**********"}
+                label={"Confirm password"}
+                {...formik.getFieldProps("passwordConfirm")}
+              />
+              {formik.errors.passwordConfirm && formik.touched.passwordConfirm ? (
+                <div className={s.errorMessage}>{formik.errors.passwordConfirm}</div>
+              ) : null}
+            </div>
           </div>
 
           <div className={s.terms}>
-            <Checkbox checked={checked} onCheckedChange={setChecked} />
+            <label className={s.container}>
+              <input name="isAgree" type="checkbox" checked={formik.values.isAgree} onChange={formik.handleChange} />
+              <span className={s.checkmark}></span>
+            </label>
 
             <Typography variant={"small"}>
               I agree to the{" "}
@@ -157,13 +190,35 @@ export default function SignUp() {
           </div>
 
           <div className={s.buttonWrapper}>
-            <Button variant={"primary"}>Sign Up</Button>
+            <Button variant={"primary"} type="submit" disabled={!formik.dirty || !formik.isValid}>
+              Sign Up
+            </Button>
             <Typography className={s.isAccount} variant={"regular_16"} textAlign={"center"}>
               Do you have an account?
             </Typography>
             <Button asChild variant={"link"}>
               <Link href={"../login"}>Sign In</Link>
             </Button>
+            {isOpen && (
+              <div className={s.popupStyles}>
+                <Cards>
+                  <div className={s.popUpHeader}>
+                    <Typography variant={"h1"} color={"light"}>
+                      Email sent
+                    </Typography>
+                    <span className={s.closeBtn} onClick={closePopup}>
+                      &times;
+                    </span>
+                  </div>
+                  <div className={s.messageAndButton}>
+                    <Typography variant={"regular_16"} color={"light"}>
+                      We have sent a link to confirm your email to {email}
+                    </Typography>
+                    <Button onClick={closePopup}>OK</Button>
+                  </div>
+                </Cards>
+              </div>
+            )}
           </div>
         </form>
       </div>
