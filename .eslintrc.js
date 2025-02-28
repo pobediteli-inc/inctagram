0d11fe1b2a1b8a3723c397a6f0aa0f43e1f432b4
umedjan kabiri <umedjan.kabiri@gmail.com>
2025-02-28 06:27:34 +0300

    SCRUM-10 feat: Enable console logs in development mode

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,7 @@ module.exports = {
   rules: {
     "react/react-in-jsx-scope": "off",
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    "no-console": "warn",
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "react/display-name": "off",
   },
 };
