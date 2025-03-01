89e7b056f1b4a344bbc707c239c6f1db472d5dfc
eliseevais <irina.eliseeva1991@gmail.com>
2025-03-01 16:16:13 +0300

    SCRUM-store: refactor store

--- src/services/inctagram.ts
+++ src/services/inctagram.ts
@@ -3,11 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const inctagramApi = createApi({
   reducerPath: 'inctagramApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://inctagram.work/api/v1/' }),
-  endpoints: (builder) => ({
-    getPostById: builder.query({
-      query: (postId) => `posts/id/${postId}`,
-    }),
-  }),
+  endpoints: () => ({}),
 })
 
-export const { useGetPostByIdQuery } = inctagramApi;
+export const {} = inctagramApi;
