dea1164285f8810c6700ce29ba0f8b49da125ee2
umedjan kabiri <umedjan.kabiri@gmail.com>
2025-03-22 04:29:50 +0300

    SCRUM-30 refactor: delete unused import

--- src/common/components/header/header.tsx
+++ src/common/components/header/header.tsx
@@ -12,7 +12,6 @@ import { authApi, useMeQuery } from "store/services/auth";
 import { useAppSelector } from "common/hooks/useAppSelector";
 import { selectIsLoggedIn, setLoggedIn } from "features/slices/auth/authSlice";
 import { useAppDispatch } from "common/hooks/useAppDispatch";
-import { selectStatus } from "features/slices/status/statusSlice";
 
 export const Header: FC = () => {
   const { data, isError, isLoading } = useMeQuery();
