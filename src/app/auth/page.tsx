eafe7c547338b06c62d4a323b5bfb5d408164416
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2025-03-27 19:39:13 +0300

    SCRUM-10 login with Google: fix rerender home page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -8,12 +8,15 @@ import { SignUpForm } from "common/components/forms";
 import {
   RegistrationArgs,
   RegistrationServerError,
-  useLoginGoogleMutation,
+  useLoginGoogleMutation, useMeQuery,
   useRegisterUserMutation,
 } from "store/services/auth";
 import { EmailSentPopup } from "./emailSentPopup/emailSentPopup";
 import { NullableProps } from "common/types";
 import { useRouter, useSearchParams } from "next/navigation";
+import { setLoggedIn } from "../../features/slices/auth/authSlice";
+import { useAppDispatch } from "../../common/hooks/useAppDispatch";
+import { handleErrors } from "../../common/utils/handleErrors";
 
 export type SignUpApiError = {
   message: string;
@@ -28,6 +31,9 @@ export default function Auth() {
   const searchParams = useSearchParams();
   const [loginGoogle] = useLoginGoogleMutation();
   const router = useRouter();
+  const { refetch } = useMeQuery();
+  const dispatch = useAppDispatch();
+
 
   const submitHandler = async (data: RegistrationArgs, resetForm: () => void) => {
     try {
@@ -58,9 +64,13 @@ export default function Auth() {
       const response = await loginGoogle({ code }).unwrap();
       if (response.accessToken) {
         localStorage.setItem("accessToken", response.accessToken);
+        dispatch(setLoggedIn({ isLoggedIn: true }));
+        await refetch();
         router.push("/home");
       }
     } catch (error) {
+      handleErrors(error, dispatch);
+      dispatch(setLoggedIn({ isLoggedIn: false }));
       console.error("Ошибка входа через Google", error);
     }
   };
