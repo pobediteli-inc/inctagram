445cadd590654f5d02195396af26907e231deb0d
umedjan kabiri <umedjan.kabiri@gmail.com>
2025-03-29 23:08:34 +0300

    SCRUM-35 feat: create User public page with styles

---
+++ src/app/publicPage/userPublicProfile/userPublicProfile.tsx
@@ -0,0 +1,9 @@
+import s from "./userProfile.module.css";
+
+export const UserPublicProfile = () => {
+  return (
+    <div className={s.mainWrapper}>
+      <div>user public profile</div>
+    </div>
+  );
+};
