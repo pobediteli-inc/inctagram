a0f74703fd7b897c9229841aca2f1c78fbac0238
umedjan kabiri <umedjan.kabiri@gmail.com>
2025-03-29 23:26:11 +0300

    SCRUM-35 feat: create Public page with styles

---
+++ src/app/publicPage/page.tsx
@@ -0,0 +1,12 @@
+import s from "./publicPage.module.css"
+import { TotalUsersCount } from "app/publicPage/totalUsersCount/totalUsersCount";
+import { UserPublicProfile } from "app/publicPage/userPublicProfile/userPublicProfile";
+
+export default function PublicPage() {
+  return (
+    <div className={s.mainWrapper}>
+      <TotalUsersCount />
+      <UserPublicProfile />
+    </div>
+  );
+}
