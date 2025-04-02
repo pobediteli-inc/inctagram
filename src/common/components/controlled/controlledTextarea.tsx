8f7c08c347d5ec25e03fff8a7565bb24080916e6
doroshkent <doroshkent@gmail.com>
2025-04-02 21:47:37 +0200

    feat: implement controlled textarea. update this component to always have caret in the end of textarea value. implement limits constant

---
+++ src/common/components/controlled/controlledTextarea.tsx
@@ -0,0 +1,44 @@
+import { FieldValues, UseControllerProps, useController } from "react-hook-form";
+import { useEffect, useRef } from "react";
+import { Textarea, TextareaProps } from "../textarea/textarea";
+
+type Props<T extends FieldValues> = Omit<TextareaProps, "onChange" | "ref" | "value"> & UseControllerProps<T>;
+
+export const ControlledTextarea = <T extends FieldValues>({
+  control,
+  defaultValue,
+  disabled,
+  name,
+  rules,
+  shouldUnregister,
+  ...textareaProps
+}: Props<T>) => {
+  const {
+    field: { onChange, value = "", ref, ...field },
+    fieldState: { error },
+  } = useController({ control, defaultValue, disabled, name, rules, shouldUnregister });
+
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+
+  useEffect(() => {
+    if (textareaRef.current) {
+      const length = textareaRef.current.value.length;
+      textareaRef.current.setSelectionRange(length, length);
+      textareaRef.current.focus();
+    }
+  }, [value]); // Перемещаем курсор в конец при изменении value
+
+  return (
+    <Textarea
+      {...textareaProps}
+      error={error?.message}
+      value={value}
+      onChange={onChange}
+      ref={(el) => {
+        ref(el);
+        textareaRef.current = el;
+      }}
+      {...field}
+    />
+  );
+};
--- src/common/components/textarea/textarea.tsx
+++ src/common/components/textarea/textarea.tsx
@@ -1,46 +1,53 @@
-import { ChangeEvent, TextareaHTMLAttributes, useState } from "react";
+import { ChangeEvent, forwardRef, TextareaHTMLAttributes, useEffect, useState } from "react";
 import clsx from "clsx";
 import s from "./textarea.module.css";
 import { Typography } from "../typography/typography";
 
-type Props = {
+export type TextareaProps = {
   title: string;
   error?: string;
   disabled?: boolean;
   maxLength?: number;
 } & TextareaHTMLAttributes<HTMLTextAreaElement>;
 
-export const Textarea = ({ title, error, disabled, maxLength, className, ...props }: Props) => {
-  const [text, setText] = useState(props.value?.toString() || "");
+export const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ title, error, disabled, maxLength, className, value = "", onChange, ...props }, ref) => {
+    const [text, setText] = useState(value.toString());
 
-  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    const newText = e.target.value;
-    setText(newText);
-    props.onChange?.(e);
-  };
+    useEffect(() => {
+      setText(value.toString());
+    }, [value]);
 
-  return (
-    <div className={s.container}>
-      <Typography variant={"regular_14"}>
-        <label className={disabled ? s.disabledLabel : s.label}>{title}</label>
-      </Typography>
-      <Typography asChild variant={"regular_16"} color={error ? "light" : "dark"}>
-        <textarea
-          disabled={disabled}
-          className={clsx(s.textarea, error && s.error, className)}
-          onChange={handleChange}
-          {...props}
-        />
-      </Typography>
-      {maxLength && (
-        <Typography variant={"small"} color={text.length > maxLength ? "error" : "dark"} className={s.charCounter}>
-          {text.length}/{maxLength}
+    const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+      const newText = e.target.value;
+      setText(newText);
+      onChange?.(e);
+    };
+
+    return (
+      <div className={s.container}>
+        <Typography variant={"regular_14"}>
+          <label className={disabled ? s.disabledLabel : s.label}>{title}</label>
         </Typography>
-      )}
-
-      <Typography variant={"regular_14"} color={"error"}>
-        {error && <p>{error}</p>}
-      </Typography>
-    </div>
-  );
-};
+        <Typography asChild variant={"regular_16"} color={error ? "light" : "dark"}>
+          <textarea
+            ref={ref}
+            disabled={disabled}
+            className={clsx(s.textarea, error && s.error, className)}
+            value={text}
+            onChange={handleChange}
+            {...props}
+          />
+        </Typography>
+        {maxLength && (
+          <Typography variant={"small"} color={text.length > maxLength ? "error" : "dark"} className={s.charCounter}>
+            {text.length}/{maxLength}
+          </Typography>
+        )}
+        <Typography variant={"regular_14"} color={"error"}>
+          {error && <p>{error}</p>}
+        </Typography>
+      </div>
+    );
+  }
+);
