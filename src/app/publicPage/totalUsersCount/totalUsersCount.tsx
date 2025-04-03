3ded2a6020d1bee7577c53c1845d9be674eca2bf
umedjan kabiri <umedjan.kabiri@gmail.com>
2025-04-03 10:01:12 +0300

    SCRUM-35 refactor: delete unused class,  rename mutation

--- src/app/publicPage/totalUsersCount/totalUsersCount.tsx
+++ src/app/publicPage/totalUsersCount/totalUsersCount.tsx
@@ -1,17 +1,17 @@
 "use client";
 import s from "./totalUsersCount.module.css";
 import { Typography } from "common/components";
-import { useGetPublicUserQuery } from "store/services/api/publicUser";
+import { useGetPublicUsersCountQuery } from "store/services/api/publicUser";
 import { Fragment } from "react";
 
 export const TotalUsersCount = () => {
-  const { data } = useGetPublicUserQuery();
+  const { data } = useGetPublicUsersCountQuery();
 
   const totalCount = data?.totalCount || 0;
   const totalCountString = totalCount.toString().padStart(6, "0");
   const totalUsers = totalCountString.split("").map((digit, index) => (
     <Fragment key={`${index}-${digit}`}>
-      <span className={s.digit}>{digit}</span>
+      <span>{digit}</span>
       {index < totalCountString.length - 1 && <span className={s.separator} />}
     </Fragment>
   ));
