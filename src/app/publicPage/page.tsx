a98f41b1aed55fbe92e07a33cd36eca5fd06285f
umedjan kabiri <umedjan.kabiri@gmail.com>
2025-04-03 10:05:47 +0300

    SCRUM-35 refactor: rename UserPublicProfile to PublicProfiles

--- src/app/publicPage/page.tsx
+++ src/app/publicPage/page.tsx
@@ -1,12 +1,12 @@
 import s from "./publicPage.module.css";
 import { TotalUsersCount } from "app/publicPage/totalUsersCount/totalUsersCount";
-import { UserPublicProfile } from "app/publicPage/userPublicProfile/userPublicProfile";
+import { PublicProfiles } from "app/publicPage/publicProfiles/publicProfiles";
 
 export default function PublicPage() {
   return (
     <div className={s.mainWrapper}>
       <TotalUsersCount />
-      <UserPublicProfile />
+      <PublicProfiles />
     </div>
   );
 }
