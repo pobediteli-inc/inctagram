969466dd48a74f4164b5c0be81ba2bfa79dff0b6
umedjan kabiri <umedjan.kabiri@gmail.com>
2025-04-03 10:03:44 +0300

    SCRUM-35 refactor: rename UserPublicProfile to PublicProfiles, update styles

--- src/app/publicPage/userPublicProfile/userPublicProfile.tsx
+++
@@ -1,9 +0,0 @@
-import s from "./userPublicProfile.module.css";
-
-export const UserPublicProfile = () => {
-  return (
-    <div className={s.mainWrapper}>
-      <div>user public profile</div>
-    </div>
-  );
-};
