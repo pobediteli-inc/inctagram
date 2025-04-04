d6a1631c981dc16bf1223b37776fd5b3727cb252
umedjan kabiri <umedjan.kabiri@gmail.com>
2025-04-05 01:03:44 +0300

    SCRUM-35 feat: implement SSR render, create getData function for fetching data from server

--- src/app/publicPage/publicProfiles/publicProfiles.tsx
+++ src/app/publicPage/publicProfiles/publicProfiles.tsx
@@ -1,14 +1,34 @@
-"use client";
 import s from "./publicProfiles.module.css";
 import { PublicProfile } from "app/publicPage/publicProfiles/publicProfile/publicProfile";
-import { useGetAllPublicPostsQuery } from "store/services/api/publicPosts";
+import { AllPublicPostsResponse } from "store/services/api/publicPosts";
+import { Typography } from "common/components";
 
-export const PublicProfiles = () => {
-  const { data } = useGetAllPublicPostsQuery();
+export const PublicProfiles = async () => {
+  const data = await getData();
+
+  if (!data)
+    return (
+      <Typography variant={"h3"} color={"light"} asChild>
+        <span>No data available</span>
+      </Typography>
+    );
 
   return (
     <div className={s.mainWrapper}>
       <PublicProfile data={data} />
     </div>
   );
+};
+
+const getData = async (): Promise<AllPublicPostsResponse | undefined> => {
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}public-posts/all`, {
+      method: "GET",
+      cache: "no-cache",
+    });
+
+    return await response.json();
+  } catch {
+    return undefined;
+  }
 };
