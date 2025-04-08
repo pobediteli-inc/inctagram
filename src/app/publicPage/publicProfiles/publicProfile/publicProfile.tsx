b0e4c2d8f7caf01dd4f45529191106fee1f90b26
umedjan kabiri <umedjan.kabiri@gmail.com>
2025-04-09 01:50:33 +0300

    SCRUM-35 feat: add 'show more/hide' functionality for profile descriptions

--- src/app/publicPage/publicProfiles/publicProfile/publicProfile.tsx
+++ src/app/publicPage/publicProfiles/publicProfile/publicProfile.tsx
@@ -1,19 +1,29 @@
+"use client";
 import s from "./publicProfile.module.css";
-import { FC, Fragment } from "react";
+import { FC, Fragment, useState } from "react";
 import { AllPublicPostsResponse } from "store/services/api/publicPosts";
 import { Typography } from "common/components";
 import { ProfileImages } from "app/publicPage/publicProfiles/publicProfile/profileImages/profileImages";
 import { CreatedDate } from "app/publicPage/publicProfiles/publicProfile/createdDate/createdDate";
 import Image from "next/image";
+import { Description } from "./description/description";
 
 export const PublicProfile: FC<Props> = ({ data }) => {
   const { items } = data ?? {};
 
+  const [isImageCollapsed, setIsImageCollapsed] = useState<Record<number, boolean>>({});
+
+  const handleShowMore = (id: number, isExpanded: boolean) =>
+    setIsImageCollapsed((prevState) => ({
+      ...prevState,
+      [id]: isExpanded,
+    }));
+
   const getItems = items?.map((item, index) => (
     <div key={`${index}-${item.id}`} className={s.mainWrapper}>
-      <ProfileImages images={item.images} />
+      <ProfileImages images={item.images} isCollapsed={isImageCollapsed[item.id] || false} />
       <div className={s.profileName}>
-        <Image
+        <Image // avatar image
           className={s.avatarOwner}
           src={item.avatarOwner || "/icons/svg/person.svg"}
           alt={item.userName}
@@ -25,7 +35,10 @@ export const PublicProfile: FC<Props> = ({ data }) => {
         </Typography>
       </div>
       <CreatedDate createdAt={item.createdAt} />
-      <div className={s.description}>{item.description}</div>
+      <Description
+        description={item.description}
+        onClickShowMore={(isExpanded) => handleShowMore(item.id, isExpanded)}
+      />
     </div>
   ));
 
