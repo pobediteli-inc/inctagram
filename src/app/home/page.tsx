42a889f322caaf6aed20faa772f300019d5c4e8f
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2025-04-09 18:49:13 +0300

    remove sidebar from home page and my profile, fix opacity for modal window "create"

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,5 @@
 import s from "./page.module.css";
-import { Sidebar } from "../../common/components";
 
 export default function Home() {
-  return (
-    <div className={s.wrapper}>
-      <Sidebar/>
-      <div className={s.home}>
-        Home
-      </div>
-    </div>
-  );
+  return <div className={s.home}>Home</div>;
 }
