f2c8cf7e5a2d2547d7111ac36b38000326f794f4
yukimurochka <uryc0n@yandex.ru>
2025-04-21 21:35:59 +0300

    SCRUM-70 some fixes

--- src/app/public-user/[profileId]/postsGrid/postsGrid.tsx
+++ src/app/public-user/[profileId]/postsGrid/postsGrid.tsx
@@ -12,18 +12,21 @@ export default function PublicProfilePostsGrid({ posts }: { posts: AllPublicPost
   return (
     <div className={s.grid}>
       {posts?.items?.length
-        ? posts.items.map((post: PostItemsResponse) => (
-            <div key={post.id} className={s.post} onClick={() => router.push(`?postId=${post.id}`)}>
-              <Image
-                src={post.images[0]?.url || defaultImage}
-                width={post.images[0]?.width || 234}
-                height={post.images[0]?.height || 228}
-                alt="Profile Post"
-                className={post.images[0]?.url ? s.image : s.defaultImage}
-                priority
-              />
-            </div>
-          ))
+        ? posts.items.map((post: PostItemsResponse) => {
+            const firstImage = post.images[0];
+            return (
+              <div key={post.id} className={s.post} onClick={() => router.push(`?postId=${post.id}`)}>
+                <Image
+                  src={firstImage?.url || defaultImage}
+                  width={firstImage?.width || 234}
+                  height={firstImage?.height || 228}
+                  alt="Profile Post"
+                  className={firstImage?.url ? s.image : s.defaultImage}
+                  priority
+                />
+              </div>
+            );
+          })
         : null}
     </div>
   );
