f5a8d8e80a6c683751be8c801fee0664ab8d5157
umedjan kabiri <umedjan.kabiri@gmail.com>
2025-04-21 17:08:23 +0300

    SCRUM-46: Refactor profile components for improved structure and styling.

--- src/app/publicPage/publicProfiles/publicProfile/publicProfile.tsx
+++ src/app/publicPage/publicProfiles/publicProfile/publicProfile.tsx
@@ -21,24 +21,25 @@ export const PublicProfile: FC<Props> = ({ data }) => {
       ...prevState,
       [id]: isExpanded,
     }));
-  const handleUserProfile = (ownerId: number) => {
-    router.push(`public-user/${ownerId}`);
-  };
+  const handleUserProfile = (ownerId: number) => router.push(`public-user/${ownerId}`);
 
   const getItems = items?.map((item, index) => (
     <div key={`${index}-${item.id}`} className={s.mainWrapper}>
       <ProfileImages images={item.images} isCollapsed={isImageCollapsed[item.id] || false} />
       <div className={s.profileName}>
-        <Image // avatar image
-          className={s.avatarOwner}
-          src={item.avatarOwner || "/icons/svg/person.svg"}
-          alt={item.userName}
-          width={36}
-          height={36}
-          priority
-        />
-        <Button variant={"link"} key={`${index}-${item.id}`} onClick={() => handleUserProfile(item.ownerId)}>
-          <Typography variant={"h3"} color={"light"} textAlign={"center"}>
+        <div className={s.avatarOwner}>
+          <Button style={{ all: "unset" }} key={`${index}-${item.id}`} onClick={() => handleUserProfile(item.ownerId)}>
+            <Image // avatar image
+              src={item.avatarOwner || "/icons/svg/person.svg"}
+              alt={item.userName}
+              width={36}
+              height={36}
+              priority
+            />
+          </Button>
+        </div>
+        <Button style={{ all: "unset" }} key={`${index}-${item.id}`} onClick={() => handleUserProfile(item.ownerId)}>
+          <Typography variant={"h3"} color={"light"} textAlign={"left"}>
             {item.userName}
           </Typography>
         </Button>
