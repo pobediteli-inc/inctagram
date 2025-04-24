fa9cf5c34dadcdfd5271d949b36815a31235a257
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2025-04-24 17:17:22 +0300

    add EmblaCarousel

--- src/app/create/imagePreview/imagePreview.tsx
+++ src/app/create/imagePreview/imagePreview.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import s from "./imagePreview.module.css";
-import "swiper/css";
 import { Close, ImageOutline } from "assets/icons";
+import useEmblaCarousel from "embla-carousel-react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 type ImagePreviewProps = {
   previewUrls: string[];
@@ -20,6 +20,25 @@ export const ImagePreview = ({
 }: ImagePreviewProps) => {
   const mainImageUrl = previewUrls[mainImageIndex];
 
+  const [emblaRef, emblaApi] = useEmblaCarousel({ align: "start", containScroll: "trimSnaps" });
+
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    emblaApi.scrollTo(mainImageIndex);
+
+    const onSelect = () => {
+      const index = emblaApi.selectedScrollSnap();
+      setMainImageIndex(index);
+    };
+
+    emblaApi.on("select", onSelect);
+
+    return () => {
+      emblaApi.off("select", onSelect);
+    };
+  }, [emblaApi, mainImageIndex, setMainImageIndex]);
+
   if (previewUrls.length === 0 || !mainImageUrl) {
     return (
       <div className={s.imageEmpty}>
@@ -30,37 +49,38 @@ export const ImagePreview = ({
 
   return (
     <div className={s.previewContainer}>
-      {mainImageUrl && (
-        <Image
-          className={s.mainImage}
-          src={previewUrls[mainImageIndex]}
-          alt="Main Preview"
-          priority
-          width={400}
-          height={400}
-        />
-      )}
+      <div className={s.mainImageWrapper}>
+        <Image className={s.mainImage} src={mainImageUrl} alt="Main Preview" priority width={400} height={400} />
+      </div>
 
       {previewUrls.length > 1 && (
-        <Swiper spaceBetween={10} slidesPerView={3} className={s.carousel}>
-          {previewUrls.map((url, index) => (
-            <SwiperSlide key={index}>
-              <div className={s.previewImageContainer}>
-                <Image
-                  className={s.previewImage}
-                  src={url}
-                  alt={`Preview ${index + 1}`}
-                  width={100}
-                  height={100}
-                  onClick={() => setMainImageIndex(index)}
-                />
-                <button className={s.removeBtn} onClick={() => handleRemoveImage(index)}>
-                  <Close width={12} height={12} />
-                </button>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        <div className={s.carouselWrapper}>
+          <button className={s.arrow + " " + s.leftArrow} onClick={() => emblaApi?.scrollPrev()}>
+            <ChevronLeft size={20} />
+          </button>
+
+          <div className={s.embla} ref={emblaRef}>
+            <div className={s.emblaContainer}>
+              {previewUrls.map((url, index) => (
+                <div className={s.emblaSlide} key={index}>
+                  <div
+                    className={`${s.previewImageContainer} ${index === mainImageIndex ? s.active : ""}`}
+                    onClick={() => setMainImageIndex(index)}
+                  >
+                    <Image className={s.previewImage} src={url} alt={`Preview ${index + 1}`} width={100} height={100} />
+                    <button className={s.removeBtn} onClick={() => handleRemoveImage(index)}>
+                      <Close width={12} height={12} />
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <button className={s.arrow + " " + s.rightArrow} onClick={() => emblaApi?.scrollNext()}>
+            <ChevronRight size={20} />
+          </button>
+        </div>
       )}
     </div>
   );
