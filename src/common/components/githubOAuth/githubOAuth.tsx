39b524e89ae84ad587f8924c3f02a0c4bdf4d22c
umedjan kabiri <umedjan.kabiri@gmail.com>
2025-04-25 04:21:11 +0300

    Refactor OAuth components to use status selector

--- src/common/components/githubOAuth/githubOAuth.tsx
+++ src/common/components/githubOAuth/githubOAuth.tsx
@@ -2,16 +2,17 @@
 
 import { useRouter, useSearchParams } from "next/navigation";
 import { FC, useEffect } from "react";
-import { setLoggedIn, setStatus } from "store/services/slices";
+import { selectStatus, setLoggedIn, setStatus } from "store/services/slices";
 import { authApi, useMeQuery } from "store/services/api/auth";
 import { handleErrors } from "common/utils";
-import { useAppDispatch } from "common/hooks";
+import { useAppDispatch, useAppSelector } from "common/hooks";
 
 export const GithubOAuth: FC<Props> = ({ redirect }) => {
   const { refetch } = useMeQuery();
   const params = useSearchParams();
   const router = useRouter();
   const dispatch = useAppDispatch();
+  const status = useAppSelector(selectStatus);
 
   const accessToken = params.get("accessToken");
 
@@ -23,13 +24,14 @@ export const GithubOAuth: FC<Props> = ({ redirect }) => {
         dispatch(authApi.util.resetApiState());
         refetch();
         router.push(redirect);
-        dispatch(setStatus({ status: "success", message: "Successfully logged in via github account." }));
+        if (status.status === "success")
+          dispatch(setStatus({ status: "success", message: "Successfully logged in via github account." }));
       } catch (error) {
         handleErrors(error, dispatch);
         dispatch(setLoggedIn({ isLoggedIn: false }));
       }
     }
-  }, [accessToken, dispatch, redirect, refetch, router]);
+  }, [accessToken, dispatch, redirect, refetch, router, status.status]);
 
   return null;
 };
