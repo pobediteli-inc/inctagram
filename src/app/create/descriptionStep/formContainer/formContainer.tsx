2368428596f8eea577f7d0ccd38cb4f3221011f8
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2025-05-14 20:56:47 +0300

    SCRUM-94 fix short import

--- src/app/create/descriptionStep/formContainer/formContainer.tsx
+++ src/app/create/descriptionStep/formContainer/formContainer.tsx
@@ -1,7 +1,7 @@
 import React, { ChangeEvent } from "react";
-import { Avatar, Textarea, Typography } from "../../../../common/components";
+import { Avatar, Textarea, Typography } from "common/components";
 import s from "./formContainer.module.css";
-import { useGetProfileQuery } from "../../../../store/services/api/profile";
+import { useGetProfileQuery } from "store/services/api/profile";
 
 type FormContainerProps = {
   description: string;
