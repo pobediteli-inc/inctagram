8fb63f8102b6e42e3b888152b4668810282e2eb2
umedjan kabiri <umedjan.kabiri@gmail.com>
2025-05-18 18:47:30 +0300

    SCRUM-97 refactor: Update icon stroke color to match currentColor

--- src/assets/icons/PaypalSvgrepoCom4.tsx
+++ src/assets/icons/PaypalSvgrepoCom4.tsx
@@ -13,7 +13,7 @@ const SvgPaypalSvgrepoCom4 = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGEle
     <g clipPath="url(#paypal-svgrepo-com-4_svg__a)">
       <path
         fill="currentColor"
-        stroke="#F3F3F3"
+        stroke="currentColor"
         d="M1.474.5h21.052c.55 0 .974.431.974.936v13.128c0 .505-.424.936-.974.936H1.474c-.55 0-.974-.431-.974-.936V1.436C.5.93.924.5 1.474.5Z"
       />
       <path
