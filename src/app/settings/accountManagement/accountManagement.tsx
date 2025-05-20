4263dc87eb9d43f3bb43107e828a6c96b5695a7b
umedjan kabiri <umedjan.kabiri@gmail.com>
2025-05-20 06:44:32 +0300

    SCRUM-97 refactor: Refactor account management to handle payment modals.

--- src/app/settings/accountManagement/accountManagement.tsx
+++ src/app/settings/accountManagement/accountManagement.tsx
@@ -1,16 +1,31 @@
 "use client";
 
-import { useState } from "react";
 import { AccountType } from "./accoutType/accountType";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { ModalPaymentProps } from "common/types";
+import { PaymentSuccessModal } from "./modalPayments/paymentSuccess";
+import { PaymentErrorModal } from "./modalPayments/paymentError";
 
 export const AccountManagement = () => {
-  const [activeModal, setActiveModal] = useState<ModalPayment>(null);
+  const searchParams = useSearchParams();
+  const [isModalOpen, setIsModalOpen] = useState<ModalPaymentProps>(null);
+  const router = useRouter();
+  useEffect(() => {
+    if (searchParams.get("success") || searchParams.get("error")) {
+      setIsModalOpen(searchParams.get("success") ? "success" : "error");
+
+      router.replace("/settings");
+    }
+  }, [router, searchParams]);
+
+  const handleClose = () => setIsModalOpen(null);
 
   return (
-    <div>
+    <>
       <AccountType />
-    </div>
+      {isModalOpen === "success" && <PaymentSuccessModal onCloseAction={handleClose} open />}
+      {isModalOpen === "error" && <PaymentErrorModal onCloseAction={handleClose} open />}
+    </>
   );
 };
-
-type ModalPayment = "success" | "error" | "create" | null;
