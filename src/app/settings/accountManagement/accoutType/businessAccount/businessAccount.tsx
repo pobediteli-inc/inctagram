1930e046bdec3966b5c9b501c46fb49034548029
umedjan kabiri <umedjan.kabiri@gmail.com>
2025-05-20 06:46:34 +0300

    SCRUM-97 feat: Add Stripe payment flow and update subscription options

--- src/app/settings/accountManagement/accoutType/businessAccount/businessAccount.tsx
+++ src/app/settings/accountManagement/accoutType/businessAccount/businessAccount.tsx
@@ -1,29 +1,44 @@
 "use client";
 
-import React from "react";
-import { RadioOptionProps } from "common/types";
+import React, { useState } from "react";
+import { BusinessAccountProps, ModalPaymentProps, RadioOptionProps } from "common/types";
 import { Card, ControlledRadioGroup, Typography } from "common/components";
 import { useForm, useWatch } from "react-hook-form";
 import s from "./businessAccount.module.css";
 import { Paypal, Stripe } from "assets/icons";
+import { CreatePaymentModal } from "../../modalPayments/createPayment";
+import { usePayment } from "./hooks/usePayment";
+import { useRouter } from "next/navigation";
+import { useAppDispatch } from "common/hooks";
+import { handleErrors } from "common/utils";
 
 export const BusinessAccount = () => {
+  const [activeModal, setActiveModal] = useState<ModalPaymentProps>(null);
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+  const { handlePayment } = usePayment();
   const { control } = useForm({
     defaultValues: {
-      businessAccount: "week" as Props,
+      businessAccount: "DAY" as BusinessAccountProps,
     },
   });
   const subscriptionType = useWatch({
     control,
     name: "businessAccount",
-    defaultValue: "week",
+    defaultValue: "DAY",
   });
 
-  const subscriptions: RadioOptionProps[] = [
-    { value: "week", label: "$10 per week" },
-    { value: "month", label: "$35 per month" },
-    { value: "year", label: "$350 per year" },
-  ];
+  const handleStripe = () => setActiveModal("create");
+  const confirmPayment = async () => {
+    setActiveModal(null);
+    try {
+      await handlePayment(subscriptionType, "STRIPE");
+      router.replace("?success=true");
+    } catch (error: unknown) {
+      router.replace("?error=true");
+      handleErrors(error, dispatch);
+    }
+  };
 
   return (
     <div className={s.businessAccountWrapper}>
@@ -40,14 +55,23 @@ export const BusinessAccount = () => {
         />
       </Card>
       <div className={s.paymentSystems}>
-        <Paypal width={96} height={64} color={"black"} />
+        <Paypal className={s.paypal} width={96} height={64} color={"var(--dark-500)"} />
         <Typography variant={"regular_14"} color={"light"}>
           Or
         </Typography>
-        <Stripe width={96} height={64} color={"black"} />
+        <Stripe className={s.stripe} width={96} height={64} color={"var(--dark-500)"} onClick={handleStripe} />
       </div>
+      <CreatePaymentModal
+        open={activeModal === "create"}
+        onCloseAction={() => setActiveModal(null)}
+        onConfirmAction={confirmPayment}
+      />
     </div>
   );
 };
 
-type Props = "week" | "month" | "year";
+const subscriptions: RadioOptionProps[] = [
+  { value: "DAY", label: "$10 per 1 day" },
+  { value: "WEEKLY", label: "$50 per week" },
+  { value: "MONTHLY", label: "$100 per month" },
+] as const;
