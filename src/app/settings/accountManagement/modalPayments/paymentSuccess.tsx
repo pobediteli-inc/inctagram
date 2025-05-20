634365977457a2a8b28ebd2f9fccc419709e04d8
umedjan kabiri <umedjan.kabiri@gmail.com>
2025-05-20 06:43:51 +0300

    SCRUM-97 refactor: Rename `close` prop to `onCloseAction` in payment modals.

--- src/app/settings/accountManagement/modalPayments/paymentSuccess.tsx
+++ src/app/settings/accountManagement/modalPayments/paymentSuccess.tsx
@@ -2,16 +2,16 @@ import { BaseModal, Button, Typography } from "common/components";
 import s from "./modalPayments.module.css";
 
 type Props = {
-  close: () => void;
+  onCloseAction: () => void;
   open: boolean;
 };
 
-export const PaymentSuccessModal = ({ open, close }: Props) => {
+export const PaymentSuccessModal = ({ open, onCloseAction }: Props) => {
   return (
-    <BaseModal open={open} onClose={close} modalTitle="Success" className={s.modalSuccess}>
+    <BaseModal open={open} onClose={onCloseAction} modalTitle="Success" className={s.modalSuccess}>
       <div className={s.modalContainer}>
         <Typography variant={"regular_16"}>Payment was successful!</Typography>
-        <Button variant={"primary"} onClick={close} className={s.button}>
+        <Button variant={"primary"} onClick={onCloseAction} className={s.button}>
           OK
         </Button>
       </div>
