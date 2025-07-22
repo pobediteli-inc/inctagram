9b618af72f6e5c24cbf8dde4384cafe457861fc9
doroshkent <doroshkent@gmail.com>
2025-07-22 19:59:30 +0200

    Add users API integration and debounce hook
    Introduce `usersApi` with support for fetching users via query parameters such as search, pagination, and cursors. Define corresponding TypeScript types for API responses and arguments. Additionally, add a reusable `useDebounce` hook for managing debounced state updates.

---
+++ src/store/services/api/users/usersApi.types.ts
@@ -0,0 +1,33 @@
+export type GetUsersArgs = {
+  search?: string;
+  pageSize?: number;
+  pageNumber?: number;
+  cursor?: number;
+};
+
+export type Avatar = {
+  url: string;
+  width: number;
+  height: number;
+  fileSize: number;
+  createdAt: string;
+};
+
+export type UserItem = {
+  id: number;
+  userName: string;
+  firstName: string;
+  lastName: string;
+  avatars: Avatar[];
+  createdAt: string;
+};
+
+export type GetUsersResponse = {
+  totalCount: number;
+  pagesCount: number;
+  page: number;
+  pageSize: number;
+  prevCursor: number;
+  nextCursor: number;
+  items: UserItem[];
+};
