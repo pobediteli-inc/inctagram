afab696916942c1aa3f86190e2822f78114b8e74
doroshkent <doroshkent@gmail.com>
2025-07-24 19:17:53 +0200

    Add user profile queries and follow/unfollow mutations
    Introduced `getUser` query, as well as `followUser` and `unfollowUser` mutations to the `usersApi`. Updated associated types to support fetching user profiles and managing follow/unfollow actions. These changes enhance the API with user interaction capabilities.

--- src/store/services/api/users/usersApi.types.ts
+++ src/store/services/api/users/usersApi.types.ts
@@ -5,6 +5,18 @@ export type GetUsersArgs = {
   cursor?: number;
 };
 
+export type GetUserArgs = {
+  userName: string;
+};
+
+export type FollowUserArgs = {
+  selectedUserId: number;
+};
+
+export type UnfollowUserArgs = {
+  userId: number;
+};
+
 export type Avatar = {
   url: string;
   width: number;
@@ -30,4 +42,22 @@ export type GetUsersResponse = {
   prevCursor: number;
   nextCursor: number;
   items: UserItem[];
+};
+
+export type UserProfile = {
+  id: number;
+  userName: string;
+  firstName: string;
+  lastName: string;
+  city: string;
+  country: string;
+  region: string;
+  dateOfBirth: Date;
+  aboutMe: string;
+  avatars: Avatar[];
+  isFollowing: boolean;
+  isFollowedBy: boolean;
+  followingCount: number;
+  followersCount: number;
+  publicationsCount: number;
 };
