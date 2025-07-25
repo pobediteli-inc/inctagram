0a5461a0381762674948e6d443553b8c2bf5965a
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2025-07-25 22:16:20 +0300

    remove console.log

--- src/common/hooks/useHandleAddComment.ts
+++ src/common/hooks/useHandleAddComment.ts
@@ -1,13 +1,16 @@
 import { useCreateCommentMutation } from "store/services/api/posts/postsApi";
+import { handleErrors } from "common/utils";
+import { useAppDispatch } from "common/hooks/useAppDispatch";
 
 export const useHandleAddComment = () => {
   const [createComment] = useCreateCommentMutation();
+  const dispatch = useAppDispatch();
 
   const handleAddComment = async (postId: number, commentText: string) => {
     try {
       await createComment({ postId, content: commentText }).unwrap();
     } catch (error) {
-      console.error("Error while creating comment:", error);
+      handleErrors(error, dispatch);
     }
   };
 
